643f600e9681be5959e5dd8738a939ed8f7da564
github tsubasashimazaki <github>
2020-07-06 14:43:52 +0900

    new file: 掲示板動作確認 modified: index.php　UI/UX(ループコンテンツまで)

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
+<?php 
+require('templates/header.php');
+?>
 
-<h1>ログイン成功</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <form>
+              <div class="form-group">
+                <label for="inputMessage">メッセージを入力</label>
+                <textarea class="form-control" id="inputMessage" rows="3"></textarea>
+                <div class="postButton"><button type="submit" class="btn btn-primary mt-3">投稿する</button></div>
+              </div>
+            </form>
+        </div>
+    </div>
+    <div class="threadContent row">
+        <div class="col-md-12 ">
+            <div class="threadArea">
+                <div class="threadMessages">
+                    <img src="./img/cat.png"  alt="">
+                    <p class="memberName"> : くるみ </p>
+                    <p class="messageTime">2020/07/05/20:45</p>
+                </div>  
+                <p class="userMessage col-md-11">こんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわすこんにちわす</p>
+            </div><!-- /threadArea[roop] -->
+        </div>
+    </div>
+</div>
+
+<?php require('templates/footer.php'); ?>
